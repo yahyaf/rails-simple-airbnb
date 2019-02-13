277f17504ed9bdc58255fdbb62b59dd9db390785
Yahya Fallah <yahya.fallah.araghi@gmail.com>
2019-02-13 18:23:51 +0100

    s6d3e3 in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'A la maison',
+  address: '11 rue des Sol Paris FRANCE',
+  description: 'Appartement super cool. On adore',
+  price_per_night: 150,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'A la desk',
+  address: '14 rue des Sol Paris FRANCE',
+  description: 'Maison super cool. On adore',
+  price_per_night: 120,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'A la Usine',
+  address: '18 rue des Sol Paris FRANCE',
+  description: 'Usine super cool. On adore',
+  price_per_night: 90,
+  number_of_guests: 12
+)
